e9c4d5e9d9522041e4830d481d851fefc5fe87f3
Chenlf-666 <52404892+Chenlf-666@users.noreply.github.com>
2019-07-04 00:04:33 +0800

    add testcase

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest,configparser
+import time
 
 class test_login(unittest.TestCase):
     @classmethod
@@ -14,10 +15,11 @@ class test_login(unittest.TestCase):
         self.driver = webdriver.Chrome()
 
     def setUp(self) -> None:
-        print("Test start")
+        print('\n' + "Test start====================")
         self.driver.get(self.baseurl)
+        # self.driver.maximize_window()
 
-    def test_login_success(self):
+    def test_1_login_success(self):
         # time.sleep(5)
         username, passwd = self.username, self.passwd
         driver = self.driver
@@ -26,12 +28,34 @@ class test_login(unittest.TestCase):
         button_login = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'登录')]")))
         button_login.click()
         userinput = wait.until(EC.visibility_of_element_located((By.ID, "username")))
-        title = driver.title
         # self.assertEqual(title, 'PAXSTORE', 'title is wrong!')
+        userinput.clear()
         userinput.send_keys(username)
+        driver.find_element_by_id("password").clear()
         driver.find_element_by_id("password").send_keys(passwd)
         driver.find_element_by_id("submitBtn").click()
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "AppListView-app-list-row")))
+        assert "chentest" in driver.page_source,"login failed"
+        time.sleep(5)
+
+    def test_2_login_fail(self):
+        # time.sleep(5)
+        username, passwd = self.username, self.passwd
+        driver = self.driver
+        wait = WebDriverWait(driver, 5)
+        button_login = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'登录')]")))
+        button_login.click()
+        userinput = wait.until(EC.visibility_of_element_located((By.ID, "username")))
+        # self.assertEqual(title, 'PAXSTORE', 'title is wrong!')
+        userinput.clear()
+        userinput.send_keys(username)
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("hahaha")
+        driver.find_element_by_id("submitBtn").click()
+        error_alert = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "alert-error")))
+        error_content = error_alert.text
+        self.assertEquals(error_content, "无效的登录信息", "页面源码中不存在该关键字")
+        # assert "无效的登录信息" in driver.page_source, "页面源码中不存在该关键字！"
 
     def get_config(self):
         config_file_name = './config/config.ini'
@@ -71,7 +95,14 @@ class test_login(unittest.TestCase):
             self.accept_next_alert = True
 
     def tearDown(self) -> None:
-        print("Test end")
+        # print(self.driver.find_element(By.CLASS_NAME, "useractionbtn").text)
+        wait = WebDriverWait(self.driver, 5)
+        if self.is_element_present(By.CLASS_NAME, "useractionbtn"):
+            print("has login in, logout")
+            self.driver.find_element(By.CLASS_NAME, "useractionbtn").click()
+            button_logout = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[contains(text(),'登出')]")))
+            button_logout.click()
+        print("Test end==============")
 
     @classmethod
     def tearDownClass(self) -> None:
