9add0356173a2840248fa18366a35da09e7e2996
Chenlf-666 <52404892+Chenlf-666@users.noreply.github.com>
2019-07-10 18:08:00 +0800

    123

--- common/LoginPage.py
+++ common/LoginPage.py
@@ -1,21 +1,26 @@
-from . import getLocator
+# from . import getLocator
 from selenium import webdriver
-import configparser
+import configparser,time
+
+from common import getLocator
 
 class LoginPage(object):
     def __init__(self, driver):
         self.driver = driver
-        config_file_name = './config/element.ini'
+        config_file_name = '../config/element.ini'
         config_file = open(config_file_name, 'r')
         self.config = configparser.ConfigParser()
         self.config.read_file(config_file)
 
     def login(self):
-        element = self.driver.find_element(getLocator(self.config.get("loginPage", "login")))
+        print(self.driver)
+        print(self.config)
+        print(getLocator.getLocator(self.config.get("loginPage", "login")))
+        element = self.driver.find_element(getLocator.getLocator(self.config.get("loginPage", "login")))
         return element
 
     def username(self):
-        element = self.driver.find_element(getLocator(self.config.get("loginPage", "user")))
+        element = self.driver.find_element(getLocator.getLocator(self.config.get("loginPage", "user")))
         return element
 
     def passwd(self):
@@ -25,3 +30,23 @@ class LoginPage(object):
     def submit(self):
         element = self.driver.find_element(getLocator(self.config.get("loginPage", "submit")))
         return element
+
+    def keyword(self):
+        print(self.config.get("loginPage", "kw"))
+        element = self.driver.find_element(getLocator.getLocator(self.config.get("loginPage", "kw")))
+        return element
+
+    def submit2(self):
+        element = self.driver.find_element(getLocator.getLocator(self.config.get("loginPage", "su")))
+        return element
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.get("https://www.baidu.com")
+    page = LoginPage(driver)
+    kw = page.keyword()
+    kw.input("123123")
+    sub = page.submit2()
+    sub.click()
+    time.sleep(30)
+    driver.close()
--- common/getLocator.py
+++ common/getLocator.py
@@ -5,9 +5,14 @@ def getLocator(locater):
         key, value = locater.split(">")
     except:
         print("配置文件格式错误")
+    print(key, value)
     if key == "id":
-        return By.id, value
+        return By.ID, value
     if key == "class":
         return By.CLASS_NAME, value
     if key == "xpath":
         return By.XPATH, value
+
+
+def test():
+    print("ggggggggggggggggggggg")
