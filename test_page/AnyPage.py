a0c41aad0aa53f7b33fcb80b3a07fe5d50259855
Chenlf-666 <52404892+Chenlf-666@users.noreply.github.com>
2019-07-13 00:54:53 +0800

    home change

--- test_page/AnyPage.py
+++ test_page/AnyPage.py
@@ -1,33 +1,41 @@
 # -*- coding: utf-8 -*-
-from common import getLocator,CommonConf
+from common import CommonConf
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import configparser,traceback
+from selenium.webdriver.common.by import By
+import configparser,traceback,time
 
 class AnyPage:
     def __init__(self, driver):
         self.driver = driver
-        self.logger = CommonConf.get_logger()
-        config_file_name = './config/element.ini'
-        self.config = configparser.ConfigParser()
-        self.config.read(config_file_name, encoding="utf-8-sig")
+        self.initConf = CommonConf.ParseConfig()
+        self.logger = self.initConf.get_logger()
         self.wait = WebDriverWait(self.driver, 5)
 
-    def userinfo(self):
+    def button_userinfo(self):
         try:
-            key, value = getLocator.getLocInfo(self.config.get("anyPage", "userinfo"))
+            # key, value = getLocator.getLocInfo(self.config.get("anyPage", "userinfo"))
+            key, value = self.initConf.getLocInfo("anyPage", "userinfo")
+            element = self.driver.find_element(key, value)
         except:
             self.logger.error("get element error===>", key, "---", value)
             self.logger.error(traceback.format_exc())
-        element = self.driver.find_element(key, value)
         return element
 
-    def logout(self):
+    def button_logout(self):
         try:
-            key, value = getLocator.getLocInfo(self.config.get("anyPage", "logout"))
+            # key, value = getLocator.getLocInfo(self.config.get("anyPage", "logout"))
+            key, value = self.initConf.getLocInfo("anyPage", "logout")
+            element = self.wait.until(EC.element_to_be_clickable((key, value)))
         except:
             self.logger.error("get element error===>", key, "---", value)
             self.logger.error(traceback.format_exc())
         # element = self.driver.find_element(key, value)
-        element = self.wait.until(EC.element_to_be_clickable((key, value)), "Get LogoutButton Time Out")
         return element
+
+    def Action_logout(self):
+        button_userinfo = self.button_userinfo()
+        button_userinfo.click()
+        button_logout = self.button_logout()
+        time.sleep(1)
+        button_logout.click()
